5adf0ff3af0810fa6bcaa27de3a05f6ec7077e87
Amr Kahla <amrkahla6@gmail.com>
2026-06-13 13:14:42 +0300

    Return list of books

--- src/main/java/com/luv2code/books/controller/BookController.java
+++ src/main/java/com/luv2code/books/controller/BookController.java
@@ -1,14 +1,36 @@
 package com.luv2code.books.controller;
 
+import com.luv2code.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BookController {
 
-    @GetMapping("/books")
-    public String firstApi(){
-        return "Hello World";
+    private final List<Book> books = new ArrayList<>();
+
+    public BookController() {
+        initializeBooks();
+    }
+
+    private void initializeBooks(){
+        books.addAll(List.of(
+                new Book("Title 0ne","Author 1", "Category one"),
+                new Book("Title Two","Author 2", "Category Two"),
+                new Book("Title Three","Author 3", "Category Three"),
+                new Book("Title Four","Author 4", "Category Four"),
+                new Book("Title Five","Author 5", "Category Five"),
+                new Book("Title Six","Author 6", "Category Six")
+              )
+        );
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> books(){
+        return books;
     }
 
 }

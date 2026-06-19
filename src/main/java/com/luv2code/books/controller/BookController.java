2926c5678bf5053ee5ccca7bffb2db1c2e085cd2
Amr Kahla <amrkahla6@gmail.com>
2026-06-19 10:43:55 +0300

    Globle Execption handler

--- src/main/java/com/luv2code/books/controller/BookController.java
+++ src/main/java/com/luv2code/books/controller/BookController.java
@@ -130,4 +130,15 @@ public class BookController {
 
         return new ResponseEntity<>(bookErrorResponse, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler
+    public ResponseEntity<BookErrorResponse> handleExceptions(Exception exception){
+        BookErrorResponse bookErrorResponse = new BookErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                exception.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(bookErrorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
